8313ebf601eaa4bea104634b1751ec820656fc1f
adamghill <adamghill@yahoo.com>
2024-09-04 22:19:23 -0400

    More substantial test.

--- tests/dj_angles/replacer/test_replace_django_templatetags.py
+++ tests/dj_angles/replacer/test_replace_django_templatetags.py
@@ -1,10 +1,36 @@
 from dj_angles.replacer import replace_django_templatetags
 
 
-def test_extends():
+def test_typical():
     expected = """{% extends 'base.html' %}
-"""
+
+{% block content %}
+  <input />
+
+  {% include 'partial.html' %}
+
+  {% include 'another-directory/another-partial.html' %}
+
+  {% debug %}
+{% endblock content %}"""
+
     actual = replace_django_templatetags("""<dj-extends 'base.html' />
-""")
+
+<dj-block content>
+  <input />
+
+  <dj-partial />
+
+  <dj-another-directory/another-partial />
+
+  <dj-debug />
+</dj-block content>""")
+
+    assert actual == expected
+
+
+def test_extends():
+    expected = "{% extends 'base.html' %}"
+    actual = replace_django_templatetags("<dj-extends 'base.html' />")
 
     assert actual == expected
